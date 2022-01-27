3fad82d15a1390c669547a7ab491003c20300fc3
German-Camilo <german.kamilo@gmail.com>
2022-01-27 01:16:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bojtoq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bojtoq)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Products } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
